20aa90a1a2cfd3edfe9c5f58f924528018c5de99
Nathan <nathan.jeffrey@gmail.com>
2015-05-07 20:46:28 +0100

    Server listening and test client running.

--- dance_server.py
+++ dance_server.py
@@ -1,6 +1,8 @@
 __author__ = 'nathan'
 
 import socket
+import pygame, sys
+from pygame.locals import *
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 5005
@@ -14,6 +16,53 @@ valid_messages = ['up',
                   'left',
                   'right']
 
+pygame.init()
+
+FPS = 30
+fpsClock = pygame.time.Clock()
+
+# set us up the window
+DISPLAYSURF = pygame.display.set_mode((800,600), 0, 32)
+pygame.display.set_caption('Animation')
+
+WHITE = (255,255,255)
+catImg = pygame.image.load('cat.png')
+catx = 400
+caty = 300
+direction = 'right'
+
+
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print "received message:", data
+    direction, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print "received message:", direction
+
+    DISPLAYSURF.fill(WHITE)
+
+    if direction == 'right':
+        catx += 5
+        if catx == 280:
+            direction = 'down'
+    elif direction == 'down':
+        caty += 5
+        if caty == 220:
+            direction = 'left'
+    elif direction == 'left':
+        catx -= 5
+        if catx == 10:
+            direction = 'up'
+    elif direction == 'up':
+        caty -= 5
+        if caty == 10:
+            direction = 'right'
+
+    direction = ''
+
+    DISPLAYSURF.blit(catImg, (catx, caty))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    fpsClock.tick(FPS)
