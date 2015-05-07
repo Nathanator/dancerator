0ec59d75b47e80c8239ae74488e44a818882de66
Connor Shearwood <drake@lifein2d.co.uk>
2015-05-07 21:05:27 +0100

    Update test_client.py

--- test_client.py
+++ test_client.py
@@ -17,17 +17,23 @@ def main(screen):
     octave = "3"
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
+        msg = ""
         while True:
-            screen.addstr(0, 0, "Arrow keys for dance moves, A-G for notes, 3-6 to change octave. Current octave is {}. q to quit.".format(octave))
+            screen.addstr(0, 0, "Arrow keys for dance moves, A-G for notes, 3-6 to change octave. Current octave is {}. q to quit. {}                      ".format(octave, msg))
             char = screen.getch()
             if char in ARROW_KEYS:
                 sock.sendto(ARROW_KEYS[char], (IP_ADDRESS, 5005))
+                msg = "Go {}.".format(ARROW_KEYS[char])
             elif char in NOTE_KEYS:
                 sock.sendto(chr(char).upper() + octave, (IP_ADDRESS, 5005))
+                msg = "Play {}.".format(chr(char).upper() + octave)
             elif char in NUMBER_KEYS:
                 octave = chr(char)
+                msg = "Octave is now {}.".format(octave)
             elif char == ord("q"):
                 return
+            else:
+                msg = "I didn't understand."
     except KeyboardInterrupt:
         return
 
